8427d3f286d55c16760e84ad489a29d37df3411f
Paul Hofer <paul.hofer@uibk.ac.at>
2022-04-11 16:31:47 +0200

    parallel execution feature implemented; however, not working as expected yet

--- src/runner.py
+++ src/runner.py
@@ -1,8 +1,10 @@
 import os
 import shutil
+from concurrent.futures import ProcessPoolExecutor, as_completed
 
 from .journal import *
 from .jobUtils import *
+
 
 def runStudy(config, args):
     printSepline()
@@ -16,15 +18,30 @@ def runStudy(config, args):
                 return
 
             message(" " + studyName + " (active) ")
-            
-            studyDict["resDir"] = os.path.abspath(studyDict["resDir"])
-            os.mkdir(studyDict["resDir"])
-            os.chdir(studyDict["resDir"])
-        
+
+            studyDir = os.path.abspath(studyDict["resDir"])
+            studyDict["resDir"] = studyDir
+
+            os.mkdir(studyDir)
+
+            for templateFile in studyDict["replaceInstructions"]:
+                shutil.copy(templateFile, os.path.join(studyDir, templateFile))
+
+            os.chdir(studyDir)
+
             jobList = generateJobListFromConfig(studyName, studyDict)
 
-            for job in jobList:
-                job.run()
+            if args.parallel:
+                message("parallel execution")
+                nJobs = len(jobList)
+                with ProcessPoolExecutor(max_workers=nJobs) as executor:
+                    futureRes = {executor.submit(job.run()): job for job in jobList}
+
+                # for future in as_completed(futureRes):
+
+            else:
+                for job in jobList:
+                    job.run()
 
             os.chdir(upmostDir)
 
