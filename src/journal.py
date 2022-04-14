2ab1db3ad39d97cb86be6ddfa2b26ac76a93b553
Dummer Alexander <alexander.dummer@student.uibk.ac.at>
2022-04-14 11:30:44 +0200

    extended message functonality

--- src/journal.py
+++ src/journal.py
@@ -1,20 +1,33 @@
 from rich import print
 
-def printHeader( ):
+
+def printHeader():
     printSepline()
-    print( "|{:^70s}|".format( "CASTOR" ) )
-    print( "|{:^70s}|".format( "" ) )
-    print( "|{:^70s}|".format( "-- MaterialModelingToolbox --" ) )
-    print( "|{:^70s}|".format( "github.com/MAteRialMOdelingToolbox" ) ) 
-    print( "|{:^70s}|".format( "" ) )
-    printSepline() 
-
-def message( string ):
-    print( "|{:<70s}|".format( string ) ) 
+    print("|{:^70s}|".format("CASTOR"))
+    print("|{:^70s}|".format(""))
+    print("|{:^70s}|".format("-- MaterialModelingToolbox --"))
+    print("|{:^70s}|".format("github.com/MAteRialMOdelingToolbox"))
+    print("|{:^70s}|".format(""))
+    printSepline()
 
 
-def printSepline( ):
-    message( 70*"=" )
+def errorMessage(*args, **kwargs):
 
-def printLine( ):
-    message( 70*"-" )
+    message(args, style="bold red", **kwargs)
+
+
+def message(*args, **kwargs):
+
+    string = args[0]
+    for arg in args[1:]:
+        string = " ".join((string, arg))
+
+    print("|{:<70s}|".format(string), **kwargs)
+
+
+def printSepline():
+    message(70 * "=")
+
+
+def printLine():
+    message(70 * "-")
