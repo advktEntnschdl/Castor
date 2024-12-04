89fb10cd0fd390ab0c27141efa078c01af74f0b2
paul <paul.hofer@uibk.ac.at>
2024-12-04 10:23:57 +0100

    restructured package

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name="castor",
     version="0.01",
     # license="",
     description="Castor: A handy tool for performing parametric studies.",
-    packages=["src"],
     author="Paul Hofer",
     author_email="Paul.Hofer@uibk.ac.at",
+    packages=find_packages(),
+    include_package_data=True,
     entry_points={
         "console_scripts": [
             "castor=castor._castor:main",
